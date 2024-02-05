3f97d225a52ee9a4178d6fdcf36f081e9f8f69f4
bradley.davis <bradley.davis@okg.com>
2024-02-04 23:28:54 -0600

    Initial commit with spring starter

---
+++ src/main/java/com/example/webhookhandlingsystem/WebhookHandlingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.webhookhandlingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebhookHandlingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebhookHandlingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webhookhandlingsystem/WebhookHandlingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webhookhandlingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebhookHandlingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
